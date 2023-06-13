2a6412c65a56aa5868fbd2fb9a2a29271a72594e
Yaroslav Pozdeev <71565279+djwcode@users.noreply.github.com>
2023-06-13 15:49:46 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-TOKEN = "MTExNjM3MzI3NzMzODc3NTU3Mw.GtP13X.5qb5m-KMNg3PPIeAspg-LUAZqKBUljsmkgbOx0"
+TOKEN = ""
 PREFIX = "!"
-MONGO_URI = "mongodb://u0vspudl9tlo6k60igg2:bKsjD2ZI8DZJpoSyi4Ds@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bze5ksolyt7no2d?replicaSet=rs0"
+MONGO_URI = ""
 
 # EMOJI color -> 6cb9fd
 CMD_DEF = 0x6cb9fd
